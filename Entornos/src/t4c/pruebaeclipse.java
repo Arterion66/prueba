b9fe0dc0ecf2fa46feef0d1e0e357dfc326d794b
David_pereda <davidperher5@gmail.com>
2021-03-25 11:52:47 +0100

    Version 2
    He añadido 2 lieas mas
    
    Signed-off-by: David_pereda <davidperher5@gmail.com>

--- Entornos/src/t4c/pruebaeclipse.java
+++ Entornos/src/t4c/pruebaeclipse.java
@@ -3,7 +3,9 @@ package t4c;
 public class pruebaeclipse {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola");
+		System.out.println("Mundo");
+		System.out.println("Que tal");
 	}
 
 }
